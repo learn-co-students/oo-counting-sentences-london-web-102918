396f4b1c710e06b9eeab4a7803936632e16cb8cd
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-03 00:04:10 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with?(".")
   end
 
   def question?
-
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
+
+  # count number of punctuation marks && that don't have
+  # another punctuation mark next to them
+  #
 
   def count_sentences
-
+    self.scan(/[.!?]+(?=\s|\z)/).size
   end
+  
 end
